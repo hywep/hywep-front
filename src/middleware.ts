1e0b46c931c6b47142ebf5afe924b3b838745d2e
boms03 <dvboms@gmail.com>
2025-01-18 18:43:14 +0900

    feat: add middleware for user authentication and route redirection
    feat: add middleware for user authentication and route redirection
    
    - Redirect unauthenticated users from /setting to /signin.
    - Prevent authenticated users from accessing /signin and /register, redirecting them to /setting.
    - Use getUserMeLoader to check authentication status and handle routes.

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import type {NextRequest} from "next/server";
+import {NextResponse} from "next/server";
+import {getUserMeLoader} from "@/data/services/user-service";
+
+export async function middleware(request: NextRequest) {
+    const user = await getUserMeLoader();
+    const currentPath = request.nextUrl.pathname;
+
+    if (currentPath.startsWith("/setting") && !user.ok) {
+        console.log(user);
+        console.log('go back to signin');
+        return NextResponse.redirect(new URL("/signin", request.url));
+    }
+
+    if (user.ok && user.data) {
+        if (currentPath.startsWith("/signin") || currentPath.startsWith("/register")) {
+            return NextResponse.redirect(new URL("/setting", request.url));
+        }
+    }
+
+    return NextResponse.next();
+}
