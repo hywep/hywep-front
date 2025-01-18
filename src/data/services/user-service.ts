9c814026b3a90bae459f579e92a83b3916a49ab3
boms03 <dvboms@gmail.com>
2025-01-18 18:44:54 +0900

    feat: add getUserMeLoader to fetch user authentication data
    feat: add getUserMeLoader to fetch user authentication data
    
    - Implemented getUserMeLoader function to retrieve user details using a JWT.
    - Fetches the authentication token via getAuthToken and verifies it with jwtVerify.
    - Returns user information (id, name, email) if the token is valid.

---
+++ src/data/services/user-service.ts
@@ -0,0 +1,34 @@
+import {getAuthToken} from "./token-service";
+import {jwtVerify} from "jose";
+
+
+export async function getUserMeLoader() {
+    try {
+        const authToken = await getAuthToken();
+        console.log('token', authToken);
+
+        if (!authToken) return {ok: false, data: null, error: null};
+
+        const {payload} = await jwtVerify(authToken, new TextEncoder().encode(process.env.JWT_SECRET!));
+
+        if (!payload || typeof payload !== "object") {
+            return {ok: false, data: null, error: "Invalid token structure"};
+        }
+
+        return {
+            ok: true,
+            data: {
+                id: payload.id,
+                name: payload.name,
+                email: payload.email,
+            },
+            error: null,
+        };
+
+    } catch (error) {
+        console.log(error);
+        return {ok: false, data: null, error: error};
+    }
+}
+
+
