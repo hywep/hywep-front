c366125e2a9180f94af669222b9bb6c894825f92
boms03 <dvboms@gmail.com>
2025-01-18 18:49:25 +0900

    feat: add user authentication and management actions
    feat: add user authentication and management actions
    
    Register User Action:
    
    - Validates user input with Zod schema for registration fields.
    - Checks for email duplication using DynamoDB's QueryCommand with an email-index.
    - Hashes the password and saves user data in DynamoDB.
    - Generates a JWT token upon successful registration and sets it in cookies.
    - Sends Slack notifications for new registrations in the production environment.
    
    Login User Action:
    
    - Validates login credentials with Zod schema.
    - Authenticates user by comparing hashed passwords and fetching user data via DynamoDB.
    - Generates and sets a JWT token in cookies upon successful login.
    
    Update User Action:
    
    - Validates input fields for user update operations using Zod schema.
    - Filters out empty or undefined values and hashes the password if updated.
    - Updates user information dynamically in DynamoDB

--- src/data/actions/auth-actions.ts
+++ src/data/actions/auth-actions.ts
@@ -1,6 +1,15 @@
 "use server";
 import {z} from "zod";
-import {ConditionalCheckFailedException, DynamoDBClient, PutItemCommand, QueryCommand,} from "@aws-sdk/client-dynamodb";
+import {loginUserService, registerUserService, updateUserById} from "@/data/services/auth-service";
+import {cookies} from "next/headers";
+import {hashPassword} from "@/data/services/encrypt-service";
+
+const config = {
+    maxAge: 60 * 60 * 24 * 7,
+    path: "/",
+    httpOnly: true,
+    secure: false,
+}
 
 const schemaRegister = z.object({
     name: z
@@ -10,23 +19,78 @@ const schemaRegister = z.object({
     email: z
         .string()
         .email({message: "올바른 이메일 주소를 입력해주세요."}),
+    password: z
+        .string()
+        .min(1, {message: "비밀번호는 최소 4자 이상이어야 합니다."})
+        .max(20, {message: "비밀번호은 최대 15자 이하여야 합니다."}),
     majors: z
         .array(z.string())
         .min(1, {message: "최소 하나의 학과를 선택해야 합니다."}),
-    grade: z.string().min(1, {message: "학년은 필수 입력 항목입니다."}),
+    grade: z
+        .number().min(1, {message: "학년은 필수 입력 항목입니다."}),
+    tags: z
+        .array(z.string())
+        .optional(),
 });
 
-const dynamoDBClient = new DynamoDBClient({region: "ap-northeast-2"});
+const schemaLogin = z.object({
+    email: z
+        .string()
+        .email({message: "올바른 이메일 주소를 입력해주세요."}),
+    password: z
+        .string()
+        .min(1, {message: "비밀번호는 최소 1자 이상이어야 합니다."})
+});
 
-const TABLE_NAME = `hywep-users-${process.env.STAGE}`;
+const schemaUpdate = z.object({
+    name: z
+        .string()
+        .min(1, {message: "사용자 이름은 최소 1자 이상이어야 합니다."})
+        .max(20, {message: "사용자 이름은 최대 20자 이하여야 합니다."}),
+    password: z
+        .union([
+            z.string().min(4, {message: "비밀번호는 최소 4자 이상이어야 합니다."}).max(15, {message: "비밀번호는 최대 15자 이하여야 합니다."}),
+            z.literal(""), // Allow empty strings
+        ])
+        .optional(),
+    confirmPassword: z
+        .union([
+            z.string().min(4, {message: "비밀번호는 최소 4자 이상이어야 합니다."}).max(15, {message: "비밀번호는 최대 15자 이하여야 합니다."}),
+            z.literal(""),
+        ])
+        .optional(),
+    email: z
+        .string()
+        .email({message: "올바른 이메일 주소를 입력해주세요."}),
+    majors: z
+        .array(z.string())
+        .min(1, {message: "최소 하나의 학과를 선택해야 합니다."}),
+    grade: z.number().min(1, {message: "학년은 필수 입력 항목입니다."}),
+    isActive: z
+        .boolean(),
+    tags: z
+        .array(z.string())
+        .optional(),
+
+}).refine((data) => {
+    if (data.password && data.confirmPassword) {
+        return data.password === data.confirmPassword;
+    }
+    return true;
+}, {
+    message: "비밀번호가 일치하지 않습니다.",
+    path: ["confirmPassword"],
+});
 
 export async function registerUserAction(prevState: never, formData: FormData) {
 
     const validatedFields = schemaRegister.safeParse({
         name: formData.get("name"),
         email: formData.get("email"),
+        password: formData.get("password"),
         majors: formData.get("majors") ? JSON.parse(formData.get("majors") as string) : [],
-        grade: formData.get("grade"),
+        grade: Number(formData.get("grade")),
+        tags: formData.get("tags") ? JSON.parse(formData.get("tags") as string) : [],
     });
 
     if (!validatedFields.success) {
@@ -38,68 +102,16 @@ export async function registerUserAction(prevState: never, formData: FormData) {
         };
     }
 
-    console.log("성공");
+    const responseData = await registerUserService(validatedFields.data);
 
-    const {name, email, majors, grade} = validatedFields.data;
-
-    try {
-        const emailCheckResponse = await dynamoDBClient.send(
-            new QueryCommand({
-                TableName: TABLE_NAME,
-                IndexName: "email-index",
-                KeyConditionExpression: "email = :email",
-                ExpressionAttributeValues: {
-                    ":email": {S: email},
-                },
-            })
-        );
-
-        if (emailCheckResponse.Count && emailCheckResponse.Count > 0) {
-            console.error("Email already exists:", email);
-            return {
-                prevState,
-                message: "이미 존재하는 이메일입니다.",
-            };
-        }
-
-        const id = `${(new Date().getTime() - Math.floor(Math.random() * 1000))}`;
-        const createdAt = new Date().toISOString().split("T")[0];
-
-        const dynamoDBResponse = await dynamoDBClient.send(
-            new PutItemCommand({
-                TableName: TABLE_NAME,
-                Item: {
-                    id: {N: id},
-                    name: {S: name},
-                    email: {S: email},
-                    majors: {
-                        L: majors.map((major: string) => ({S: major})),
-                    },
-                    grade: {N: grade},
-                    created_at: {S: createdAt},
-                },
-            })
-        );
-
-        console.log("DynamoDB Response:", dynamoDBResponse);
-    } catch (error) {
-        if (error instanceof ConditionalCheckFailedException) {
-            console.error("DynamoDB Conditional Check Failed:", error);
-            return {
-                prevState,
-                message: "DynamoDB 조건 검사 실패: 이미 존재하는 데이터입니다.",
-            };
-        }
-
-        console.error("DynamoDB Error:", error);
+    if (!responseData.success) {
         return {
             prevState,
-            message: "DynamoDB 저장 중 오류가 발생했습니다.",
+            message: responseData.message,
         };
     }
 
-    if (process.env.STAGE === 'prod')
-        await sendSlackMessage(`${process.env.STAGE} 회원 가입:\n- 이름: ${name}\n- 이메일: ${email}\n- 학과: ${majors.join(',')}\n- 학년: ${grade}`);
+    (await cookies()).set("jwt", responseData.token!, config);
 
     return {
         prevState,
@@ -108,30 +120,103 @@ export async function registerUserAction(prevState: never, formData: FormData) {
     };
 }
 
+export async function loginUserAction(prevState: never, formData: FormData) {
 
-async function sendSlackMessage(message: string) {
-    const payload = {text: message};
+    const validatedFields = schemaLogin.safeParse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+    });
 
-    try {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        const response = await fetch(process.env.SLACK_WEBHOOK_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-        });
-
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error('Slack API error:', errorData);
-            return;
-        }
-
-        console.log('Slack message sent successfully');
-    } catch (error) {
-        console.error('Error sending Slack message:', error);
+    if (!validatedFields.success) {
+        console.log(validatedFields.error.flatten());
+        return {
+            prevState,
+            zodErrors: validatedFields.error.flatten(),
+            message: "유효성 검사 실패",
+        };
     }
+
+    const responseData = await loginUserService(validatedFields.data);
+
+    if (!responseData.success) {
+        return {
+            prevState,
+            zodErrors: {
+                formErrors: [],
+                fieldErrors: {
+                    email: ["이메일 혹은 비밀번호를 재입력하세요."]
+                },
+            },
+            message: responseData.message,
+        };
+    }
+
+    console.log(config);
+    (await cookies()).set("jwt", responseData.token!, config);
+
+    return {
+        prevState,
+        data: "ok",
+        message: "성공적으로 로그인되었습니다.",
+    };
 }
 
+export async function updateUserAction(prevState: never, formData: FormData) {
+
+    const userId = Number(formData.get("id"));
+
+    if (!userId) {
+        return {
+            prevState,
+            message: "유저 아이디가 없습니다.",
+        };
+    }
+
+    const validatedFields = schemaUpdate.safeParse({
+        name: formData.get("name"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+        confirmPassword: formData.get("confirmPassword"),
+        majors: formData.get("majors") ? JSON.parse(formData.get("majors") as string) : [],
+        grade: Number(formData.get("grade")!.toString()[0]),
+        isActive: formData.get("isActive") === 'true',
+        tags: formData.get("tags") ? JSON.parse(formData.get("tags") as string) : [],
+    });
+
+    if (!validatedFields.success) {
+        console.log(validatedFields.error.flatten());
+        return {
+            prevState,
+            zodErrors: validatedFields.error.flatten(),
+            message: "유효성 검사 실패",
+        };
+    }
+
+    const filteredData = Object.entries(validatedFields.data).reduce((acc, [key, value]) => {
+        if (key !== "confirmPassword" && value !== undefined && value !== null && value !== "") {
+            (acc as Record<string, any>)[key] = value;
+        }
+        return acc;
+    }, {} as Partial<typeof validatedFields.data>);
+
+
+    if (filteredData.password) {
+        filteredData.password = await hashPassword(filteredData.password);
+    }
+
+
+    const responseData = await updateUserById(userId, filteredData);
+
+    if (!responseData.success) {
+        return {
+            prevState,
+            message: responseData.message,
+        };
+    }
+
+    return {
+        prevState,
+        data: "ok",
+        message: "성공적으로 등록되었습니다.",
+    };
+}
